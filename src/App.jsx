3b1b60f73001e5abc9ad71157578ad52e4999702
bert114 <janalbertpenano14@gmail.com>
2023-12-28 13:04:34 +0800

    adding all files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import MyComponent from "./rate";
+import "./scss/main.scss";
+import "animate.css";
+
+function App() {
+  return <MyComponent />;
+}
+
+export default App;
---
+++ src/Rate.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import listItem from "./List";
+import img from "../src/assets/images/illustration-thank-you.svg";
+
+function ThankYou({ selectedNum }) {
+  return (
+    <div className="thank-you">
+      <img src={img} alt="thankYou" />
+      <div className="result">You selected {selectedNum} out of 5</div>
+      <h1>Thank you</h1>
+      <p>
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don’t hesitate to get in touch!
+      </p>
+    </div>
+  );
+}
+
+function Rate({ handleNum }) {
+  return (
+    <>
+      <div className="icon">
+        <div></div>
+      </div>
+      <h1>How did we do?</h1>
+      <p>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+      <ul onClick={(e) => handleNum(e)} className="btn-flex">
+        {listItem}
+      </ul>
+      <button className="primary-btn" type="submit">
+        SUBMIT
+      </button>
+    </>
+  );
+}
+
+function MyComponent() {
+  const [click, setClick] = useState(false);
+  const [selectedNum, setSelectedNum] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setClick(true);
+    handleNum();
+  };
+
+  const handleNum = (e) => {
+    setSelectedNum(e.target.id);
+  };
+
+  const condition = !click ? (
+    <Rate handleNum={(e) => handleNum(e)} />
+  ) : (
+    <ThankYou selectedNum={selectedNum} />
+  );
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit} className="container">
+        {condition}
+      </form>
+    </main>
+  );
+}
+
+export default MyComponent;
